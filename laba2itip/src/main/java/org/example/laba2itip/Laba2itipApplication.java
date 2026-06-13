16ab53c4f075baa688d01f825f47306afb14a08e
Sergey <110046935+SushenaAr@users.noreply.github.com>
2026-06-13 23:38:48 +0300

    Add files via upload

---
+++ laba2itip/src/main/java/org/example/laba2itip/Laba2itipApplication.java
@@ -0,0 +1,13 @@
+package org.example.laba2itip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba2itipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laba2itipApplication.class, args);
+    }
+
+}
---
+++ laba2itip/src/test/java/org/example/laba2itip/Laba2itipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.laba2itip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba2itipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
